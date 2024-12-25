86fe147847628784f472e882b7389b9170c81ac0
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 13:27:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function toCase(text) {
-  // write your code here
+  // Convert the text to lowercase and uppercase, and then concatenate them with a hyphen
+  return text.toLowerCase() + '-' + text.toUpperCase();
 }
 
 // DO not change the code below
